ce7208aa64289e201106b749cd4bbff0810fcf3a
gbrggrn <gustberg107@student.kau.se>
2026-01-22 13:38:21 +0100

    Register DbSet for models in ApplicationDBContext

--- VedaVerk/VedaVerk/Data/ApplicationDbContext.cs
+++ VedaVerk/VedaVerk/Data/ApplicationDbContext.cs
@@ -1,9 +1,13 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using VedaVerk.Models;
 
 namespace VedaVerk.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
-    }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Pizza> Pizzas { get; set; }
+        public DbSet<VegBag> VegBags { get; set; }
+	}
 }
