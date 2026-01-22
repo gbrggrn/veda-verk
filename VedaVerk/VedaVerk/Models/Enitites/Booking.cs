b2e95019dcda770f05dc981b7b2cda66de769c73
gbrggrn <gustberg107@student.kau.se>
2026-01-22 14:59:36 +0100

    Model properties

--- VedaVerk/VedaVerk/Models/Enitites/Booking.cs
+++ VedaVerk/VedaVerk/Models/Enitites/Booking.cs
@@ -1,6 +1,23 @@
-﻿namespace VedaVerk.Models.Enitites
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VedaVerk.Models.Enitites
 {
 	public class Booking
 	{
+		public int Id { get; set; }
+
+		// Boking details
+		[Required] public DateTime BookingDate { get; set; }
+		[Required] public Product? Product { get; set; }
+
+		// Customer details
+		[Required] public string CustomerName { get; set; } = string.Empty;
+		[Required] public string CustomerEmail { get; set; } = string.Empty;
+
+		// Status
+		public bool IsCancelled { get; set; }
+
+		// Foreign key
+		public int ProductId { get; set; }
 	}
 }
--- VedaVerk/VedaVerk/Models/Enitites/Product.cs
+++ VedaVerk/VedaVerk/Models/Enitites/Product.cs
@@ -1,6 +1,16 @@
-﻿namespace VedaVerk.Models.Enitites
+﻿using System.ComponentModel.DataAnnotations;
+using VedaVerk.Models.Enums;
+
+namespace VedaVerk.Models.Enitites
 {
 	public class Product
 	{
+		public int Id { get; set; }
+		[Required] public string Name { get; set; } = string.Empty;
+		[Required] public string Description { get; set; } = string.Empty;
+		[Required] public decimal Price { get; set; }
+		[Required] public int Capacity { get; set; }
+		public ProductType Type { get; set; }
+		public string? ImageUrl { get; set; }
 	}
 }
