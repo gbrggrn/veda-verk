0f0390e4a798a6e9346b8eccd3dd84c6d2a24afc
gbrggrn <gustberg107@student.kau.se>
2026-01-22 13:41:20 +0100

    Basic CRUD interface

--- VedaVerk/VedaVerk/Repositiories/Interfaces/IRepository.cs
+++ VedaVerk/VedaVerk/Repositiories/Interfaces/IRepository.cs
@@ -1,6 +1,11 @@
 ﻿namespace VedaVerk.Repositiories.Interfaces
 {
-	public interface IRepository
+	public interface IRepository <T> where T : class
 	{
+		Task<IEnumerable<T>> GetAllAsync();
+		Task<T?> GetByIdAsync(int id);
+		Task AddSync(T entity);
+		Task UpdateAsync(T entity);
+		Task DeleteAsync(int id);
 	}
 }
