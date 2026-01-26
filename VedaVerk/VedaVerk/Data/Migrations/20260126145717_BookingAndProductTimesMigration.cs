251d2e08f89e3d556fa55d76b428715528a30363
gbrggrn <gustberg107@student.kau.se>
2026-01-26 16:03:51 +0100

    New migration to reflect state changes in db + less ugly login-page

---
+++ VedaVerk/VedaVerk/Data/Migrations/20260126145717_BookingAndProductTimesMigration.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VedaVerk.Migrations
+{
+    /// <inheritdoc />
+    public partial class BookingAndProductTimesMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ActiveFrom",
+                table: "Products",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ActiveTo",
+                table: "Products",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "CapacityPerSlot",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "CloseTime",
+                table: "Products",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "Products",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<double>(
+                name: "IntervalMinutes",
+                table: "Products",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdated",
+                table: "Products",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "OpenTime",
+                table: "Products",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BookingTime",
+                table: "Bookings",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "Bookings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ActiveFrom",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ActiveTo",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CapacityPerSlot",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CloseTime",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "IntervalMinutes",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdated",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "OpenTime",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "BookingTime",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "Bookings");
+        }
+    }
+}
