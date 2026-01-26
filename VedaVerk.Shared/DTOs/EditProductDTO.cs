81d128f1277ab5f6e8c194d75751f195b6a89b27
gbrggrn <gustberg107@student.kau.se>
2026-01-26 14:57:31 +0100

    ProductFormModal designed + wired

--- VedaVerk.Shared/DTOs/EditProductDTO.cs
+++ VedaVerk.Shared/DTOs/EditProductDTO.cs
@@ -9,11 +9,21 @@ namespace VedaVerk.Shared.DTOs
 {
 	public class EditProductDTO
 	{
+		public int Id { get; set; }
 		public string? Name { get; set; }
 		public string? Description { get; set; }
 		public decimal Price { get; set; }
 		public int Capacity { get; set; }
 		public string? ImageUrl { get; set; }
 		public ProductType Type { get; set; }
+		public bool IsActive { get; set; }
+		public TimeSpan OpenTime { get; set; }
+		public TimeSpan CloseTime { get; set; }
+		public double IntervalMinutes { get; set; }
+		public int CapacityPerSlot { get; set; }
+		public DateTime Created { get; set; }
+		public DateTime LastUpdated { get; set; }
+		public DateTime ActiveFrom { get; set; }
+		public DateTime ActiveTo { get; set; }
 	}
 }
